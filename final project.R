a7d6917956cf0ae327f5e0305d5ea0d9934344ef
ChenfeiZhou0427 <131484370+ChenfeiZhou0427@users.noreply.github.com>
2023-05-08 19:22:50 -0700

    Update final project.R

--- final project.R
+++ final project.R
@@ -1,7 +1,7 @@
 # final project
 library(dplyr)
 
-traffic_df <- read.csv('Traffic_Crashes_-_Crashes.csv')
+traffic_df <- read.csv('Traffic_Crashes_-_Crashes 2.csv')
 weather_df <- read.csv('3311717.csv')
 
 weather_df <- select(weather_df, DATE, HourlyAltimeterSetting, HourlyDewPointTemperature, 
@@ -15,13 +15,14 @@ weather_df$Year <- substr(weather_df$DATE, 1, 4)
 weather_df$Month <- substr(weather_df$DATE, 6, 7)
 weather_df$Day <- substr(weather_df$DATE, 9, 10)
 
-traffic_df$Year <- substr(traffic_df$CRASH_DATE, 7, 10)
-traffic_df$Month <- substr(traffic_df$CRASH_DATE, 1, 2)
-traffic_df$Day <- substr(traffic_df$CRASH_DATE, 4, 5)
+traffic_df[c('Month', 'Day', 'Year')] <- str_split_fixed(traffic_df$CRASH_DATE, '/', 3)
+traffic_df$Year <- substr(traffic_df$Year, 1, 2)
 
 traffic_df$Big_damage[traffic_df$DAMAGE == 'OVER $1,500'] <- 1
 traffic_df$Big_damage[traffic_df$DAMAGE != 'OVER $1,500'] <- 0
 
-combo_df <- right_join(traffic_df, weather_df, by = c('Year', 'Month', 'Day'))
+combo_df <- merge(traffic_df, weather_df, by = c('Year', 'Month', 'Day'))
+
+write.csv(combo_df, "~/Desktop/haha/info201/Final project/combo_df.csv", row.names = FALSE)
 
 
